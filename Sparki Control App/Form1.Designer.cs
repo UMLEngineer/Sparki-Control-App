0d66851bfcffc2f648dbbeb40c34653123dae7b2
UMLEngineer <griffith.evan@gmail.com>
2015-12-29 14:15:31 -0500

    First committ, can do basic bluetooth comm communication but needs to handle timeouts better and expand functionality.

---
+++ Sparki Control App/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Sparki_Control_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbPorts = new System.Windows.Forms.ComboBox();
+            this.cbBaud = new System.Windows.Forms.ComboBox();
+            this.pbStatus = new System.Windows.Forms.ProgressBar();
+            this.laPort = new System.Windows.Forms.Label();
+            this.laBaud = new System.Windows.Forms.Label();
+            this.laStatus = new System.Windows.Forms.Label();
+            this.buPing = new System.Windows.Forms.Button();
+            this.tbDataReceived = new System.Windows.Forms.TextBox();
+            this.buOpen = new System.Windows.Forms.Button();
+            this.buClose = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.laDataReceived = new System.Windows.Forms.Label();
+            this.tbDiag = new System.Windows.Forms.TextBox();
+            this.laDiag = new System.Windows.Forms.Label();
+            this.buDemo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbPorts
+            // 
+            this.cbPorts.FormattingEnabled = true;
+            this.cbPorts.Location = new System.Drawing.Point(12, 56);
+            this.cbPorts.Name = "cbPorts";
+            this.cbPorts.Size = new System.Drawing.Size(121, 21);
+            this.cbPorts.TabIndex = 0;
+            // 
+            // cbBaud
+            // 
+            this.cbBaud.FormattingEnabled = true;
+            this.cbBaud.Items.AddRange(new object[] {
+            "9600",
+            "115200"});
+            this.cbBaud.Location = new System.Drawing.Point(160, 56);
+            this.cbBaud.Name = "cbBaud";
+            this.cbBaud.Size = new System.Drawing.Size(121, 21);
+            this.cbBaud.TabIndex = 1;
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.Location = new System.Drawing.Point(320, 54);
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(100, 23);
+            this.pbStatus.TabIndex = 2;
+            // 
+            // laPort
+            // 
+            this.laPort.AutoSize = true;
+            this.laPort.Location = new System.Drawing.Point(12, 37);
+            this.laPort.Name = "laPort";
+            this.laPort.Size = new System.Drawing.Size(26, 13);
+            this.laPort.TabIndex = 3;
+            this.laPort.Text = "Port";
+            // 
+            // laBaud
+            // 
+            this.laBaud.AutoSize = true;
+            this.laBaud.Location = new System.Drawing.Point(160, 38);
+            this.laBaud.Name = "laBaud";
+            this.laBaud.Size = new System.Drawing.Size(32, 13);
+            this.laBaud.TabIndex = 4;
+            this.laBaud.Text = "Baud";
+            // 
+            // laStatus
+            // 
+            this.laStatus.AutoSize = true;
+            this.laStatus.Location = new System.Drawing.Point(320, 36);
+            this.laStatus.Name = "laStatus";
+            this.laStatus.Size = new System.Drawing.Size(65, 13);
+            this.laStatus.TabIndex = 5;
+            this.laStatus.Text = "Connected?";
+            // 
+            // buPing
+            // 
+            this.buPing.Location = new System.Drawing.Point(15, 120);
+            this.buPing.Name = "buPing";
+            this.buPing.Size = new System.Drawing.Size(75, 23);
+            this.buPing.TabIndex = 6;
+            this.buPing.Text = "Ping Distance";
+            this.buPing.UseVisualStyleBackColor = true;
+            this.buPing.Click += new System.EventHandler(this.buPing_Click);
+            // 
+            // tbDataReceived
+            // 
+            this.tbDataReceived.Location = new System.Drawing.Point(160, 122);
+            this.tbDataReceived.Multiline = true;
+            this.tbDataReceived.Name = "tbDataReceived";
+            this.tbDataReceived.Size = new System.Drawing.Size(260, 96);
+            this.tbDataReceived.TabIndex = 7;
+            // 
+            // buOpen
+            // 
+            this.buOpen.Location = new System.Drawing.Point(475, 120);
+            this.buOpen.Name = "buOpen";
+            this.buOpen.Size = new System.Drawing.Size(75, 23);
+            this.buOpen.TabIndex = 8;
+            this.buOpen.Text = "Open";
+            this.buOpen.UseVisualStyleBackColor = true;
+            this.buOpen.Click += new System.EventHandler(this.buOpen_Click);
+            // 
+            // buClose
+            // 
+            this.buClose.Location = new System.Drawing.Point(475, 149);
+            this.buClose.Name = "buClose";
+            this.buClose.Size = new System.Drawing.Size(75, 23);
+            this.buClose.TabIndex = 9;
+            this.buClose.Text = "Close";
+            this.buClose.UseVisualStyleBackColor = true;
+            this.buClose.Click += new System.EventHandler(this.buClose_Click);
+            // 
+            // laDataReceived
+            // 
+            this.laDataReceived.AutoSize = true;
+            this.laDataReceived.Location = new System.Drawing.Point(160, 103);
+            this.laDataReceived.Name = "laDataReceived";
+            this.laDataReceived.Size = new System.Drawing.Size(79, 13);
+            this.laDataReceived.TabIndex = 10;
+            this.laDataReceived.Text = "Data Received";
+            // 
+            // tbDiag
+            // 
+            this.tbDiag.Location = new System.Drawing.Point(160, 269);
+            this.tbDiag.Multiline = true;
+            this.tbDiag.Name = "tbDiag";
+            this.tbDiag.Size = new System.Drawing.Size(260, 114);
+            this.tbDiag.TabIndex = 11;
+            // 
+            // laDiag
+            // 
+            this.laDiag.AutoSize = true;
+            this.laDiag.Location = new System.Drawing.Point(160, 250);
+            this.laDiag.Name = "laDiag";
+            this.laDiag.Size = new System.Drawing.Size(107, 13);
+            this.laDiag.TabIndex = 12;
+            this.laDiag.Text = "Diagnotic Information";
+            // 
+            // buDemo
+            // 
+            this.buDemo.Location = new System.Drawing.Point(15, 148);
+            this.buDemo.Name = "buDemo";
+            this.buDemo.Size = new System.Drawing.Size(75, 23);
+            this.buDemo.TabIndex = 13;
+            this.buDemo.Text = "Demo";
+            this.buDemo.UseVisualStyleBackColor = true;
+            this.buDemo.Click += new System.EventHandler(this.buDemo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 408);
+            this.Controls.Add(this.buDemo);
+            this.Controls.Add(this.laDiag);
+            this.Controls.Add(this.tbDiag);
+            this.Controls.Add(this.laDataReceived);
+            this.Controls.Add(this.buClose);
+            this.Controls.Add(this.buOpen);
+            this.Controls.Add(this.tbDataReceived);
+            this.Controls.Add(this.buPing);
+            this.Controls.Add(this.laStatus);
+            this.Controls.Add(this.laBaud);
+            this.Controls.Add(this.laPort);
+            this.Controls.Add(this.pbStatus);
+            this.Controls.Add(this.cbBaud);
+            this.Controls.Add(this.cbPorts);
+            this.Name = "Form1";
+            this.Text = "Sparki Control Center";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbPorts;
+        private System.Windows.Forms.ComboBox cbBaud;
+        private System.Windows.Forms.ProgressBar pbStatus;
+        private System.Windows.Forms.Label laPort;
+        private System.Windows.Forms.Label laBaud;
+        private System.Windows.Forms.Label laStatus;
+        private System.Windows.Forms.Button buPing;
+        private System.Windows.Forms.TextBox tbDataReceived;
+        private System.Windows.Forms.Button buOpen;
+        private System.Windows.Forms.Button buClose;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label laDataReceived;
+        private System.Windows.Forms.TextBox tbDiag;
+        private System.Windows.Forms.Label laDiag;
+        private System.Windows.Forms.Button buDemo;
+    }
+}
+
---
+++ Sparki Control App/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace Sparki_Control_App
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            getAvailablePorts();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void getAvailablePorts()
+        {
+            String[] ports = SerialPort.GetPortNames();
+            cbPorts.Items.AddRange(ports);
+
+        }
+
+        private void buOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cbPorts.Text == "" || cbBaud.Text == "")
+                {
+                    tbDataReceived.Text = "Please select port settings";
+                }
+                else
+                {
+                    serialPort1.PortName = cbPorts.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(cbBaud.Text);
+                    serialPort1.Open();
+                    pbStatus.Value = 100;
+                    buClose.Enabled = true;
+                    buOpen.Enabled = false;
+                    buPing.Enabled = true;
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                tbDataReceived.Text = "Unauthorized Access";
+            }
+        }
+
+        private void buClose_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            pbStatus.Value = 0;
+            buOpen.Enabled = true;
+            buClose.Enabled = false;
+            buPing.Enabled = false;
+        }
+        string commandOut;
+
+        int checkSumF(char[] arr)
+        {
+            int sum = 0;
+            for (int i=0; i < 5; i++)
+                sum += arr[i];
+            return sum;
+        }
+
+        void commandCreater(String commOut)
+        {
+            char[] commandArray = new char[9];
+            commandArray = commOut.ToCharArray();
+            int checkSumV = checkSumF(commandArray);
+            commOut += checkSumV;
+            commOut += 'z';
+            serialPort1.Write(commOut);
+            tbDiag.Text = "Data Sent: " + commOut;
+        }
+
+        private void buPing_Click(object sender, EventArgs e)
+        {
+            commandCreater("000pin");
+
+            try
+            {
+                tbDataReceived.Text = serialPort1.ReadLine();
+            }
+            catch (TimeoutException)
+            {
+                tbDiag.Text = "Timeout Excetion";
+            }
+        }
+
+         private void buDemo_Click(object sender, EventArgs e)
+        {
+            commandCreater("090lef");
+        }
+    }
+    }
